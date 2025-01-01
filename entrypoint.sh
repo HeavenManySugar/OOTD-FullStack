ef8e31c7e3be1d4c0da2d66752b5136cebc7d370
ZRE <chy853@gmail.com>
2025-01-02 07:50:07 +0800

    Refactor Docker Compose configuration and add entrypoint script for MSSQL server initialization

---
+++ entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+waiting_time=30
+
+# wait for MSSQL server to start
+export STATUS=1
+i=0
+
+sleep $waiting_time
+while [[ $STATUS -ne 0 ]] && [[ $i -lt $waiting_time ]];
+do
+	i=$i+1
+	/opt/mssql-tools18/bin/sqlcmd -t 1 -U sa -P $MSSQL_SA_PASSWORD -C -Q "select 1" >> /dev/null
+	STATUS=$?
+done
+
+if [ $STATUS -ne 0 ]; then 
+	echo "======= Error: MSSQL SERVER took more than $waiting_time seconds to start up.  ========";
+	exit 1
+fi
+
+echo "======= MSSQL SERVER STARTED ========"
+
+/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P Abcd1234 -C -Q "IF DB_ID('OOTDV1') IS NULL BEGIN RESTORE DATABASE [OOTDV1] FROM DISK = '/var/opt/mssql/backup/OOTDV2.bak' WITH MOVE 'OOTDV1' TO '/var/opt/mssql/data/OOTDV1.mdf', MOVE 'OOTDV1_log' TO '/var/opt/mssql/data/OOTDV1_log.ldf' END"
+
+echo "======= MSSQL CONFIG COMPLETE ======="
