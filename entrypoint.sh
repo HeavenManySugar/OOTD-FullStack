217d5cd6057b1828c23dc9c7c8b172c4293bd2b3
ZRE <chy853@gmail.com>
2025-01-02 19:50:08 +0800

    Update backend subproject and adjust database backup file references in compose.yml and entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,6 +20,6 @@ fi
 
 echo "======= MSSQL SERVER STARTED ========"
 
-/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P Abcd1234 -C -Q "IF DB_ID('OOTDV1') IS NULL BEGIN RESTORE DATABASE [OOTDV1] FROM DISK = '/var/opt/mssql/backup/OOTDV2.bak' WITH MOVE 'OOTDV1' TO '/var/opt/mssql/data/OOTDV1.mdf', MOVE 'OOTDV1_log' TO '/var/opt/mssql/data/OOTDV1_log.ldf' END"
+/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P Abcd1234 -C -Q "IF DB_ID('OOTDV1') IS NULL BEGIN RESTORE DATABASE [OOTDV1] FROM DISK = '/var/opt/mssql/backup/OOTD.bak' WITH MOVE 'OOTDV1' TO '/var/opt/mssql/data/OOTDV1.mdf', MOVE 'OOTDV1_log' TO '/var/opt/mssql/data/OOTDV1_log.ldf' END"
 
 echo "======= MSSQL CONFIG COMPLETE ======="
